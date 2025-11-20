c89c52316941cfcd371e08bc899f0b62b2e16672
FlowerGrid <FlowerGridCoding@gmail.com>
2025-11-19 22:38:42 -0600

    Display whose turn it is Implement coin toss to select who plays first

--- static/ticTacToe.js
+++ static/ticTacToe.js
@@ -1,10 +1,10 @@
 // TODO:
-// - New game button resets the game board, doesn't effect scores
-// - Reset terminates the current game and creat4es a new game with new players
-//   and zeroes out scores
-// - Need to add event listeners to menu buttons and marker spaces
-// - Cllicking on a marker space indexes into that space on the board array and 
-//   fills it with that player's marker
+// - New game button resets the game board, doesn't effect scores ✅
+// - Reset terminates the current game and creates a new game with new players
+//   and zeroes out scores ✅
+// - Need to add event listeners to menu buttons and marker spaces ✅
+// - Clicking on a marker space indexes into that space on the board array and 
+//   fills it with that player's marker ✅
 // - Need to display whose turn it is
 //   - I might highlight the player's name in the menu or simply write it above
 //     the game board
@@ -38,15 +38,131 @@ const pubsub = {
   }
 };
 
+
+// UI Manager
+(function () {
+    // Locate important html elements
+    const gameControlsElement = document.querySelector('.game-controls');
+    const newGameBtn = document.querySelector('#new-game-btn');
+    const resetBtn = document.querySelector('#reset-btn');
+    const gameBoardElement = document.querySelector('#game-board');
+    const p1Element = document.querySelector('#player1');
+    const p2Element = document.querySelector('#player2');
+
+    // Listenters
+    gameBoardElement.addEventListener('click', (event) => {
+    let target = event.target;
+    let spaceIdx = target.dataset.spaceIdx;
+    pubsub.publish("spaceClicked", spaceIdx)
+    return spaceIdx
+    });
+
+    gameControlsElement.addEventListener('mousedown', (event) => {
+        let target = event.target;
+        target.classList.toggle('btn-dwn');
+    });
+
+    gameControlsElement.addEventListener('mouseup', (event) => {
+        let target = event.target;
+        target.classList.toggle('btn-dwn');
+        if(target.id === 'new-game-btn') {
+            pubsub.publish('btnPressed', 'newGame');
+        } else if (target.id === 'reset-btn') {
+            pubsub.publish('btnPressed', 'reset');
+        }
+        
+    });
+
+    const markSpace = (board) => {
+        for (let i = 0; i < board.length; i++){
+            let boardSpaceElement = document.querySelector(`#marker-space${i}`);
+            boardSpaceElement.classList.remove('x-marker', 'o-marker');
+            if (board[i]) {
+                boardSpaceElement.classList.toggle(`${board[i]}-marker`);
+            }
+        }};
+
+    const updateScore = (player) => {
+        let id = extractPlayerId(player);
+        let playerScore = document.querySelector(`#p${id}-score`);
+        playerScore.textContent = player.getScore()
+    }
+
+    function extractPlayerId(player) {
+        return player.getplayerId()
+    }
+
+    // Pubsub Subscriptions
+    pubsub.subscribe('boardChanged', (board) => {
+        markSpace(board)
+    });
+
+    pubsub.subscribe('win', (players) => {
+        players.forEach((player) => {
+            updateScore(player);
+        })
+    });
+
+    pubsub.subscribe('gameStateChange', (payload) => {
+        // updateScore(payload.players);
+        payload.players.forEach((player) => {
+            updateScore(player);
+        })
+        markSpace(payload.clearedBoard);
+    })
+
+    pubsub.subscribe('turnChange', (data) => {
+        for (let playerElement of [p1Element, p2Element]) {
+            playerElement.classList.toggle('my-turn');
+        }
+    })
+
+    pubsub.subscribe('coinToss', (id) => {
+        console.log(id)
+        for (let playerElement of [p1Element, p2Element]) {
+            playerElement.classList.remove('my-turn');
+        }
+        if (id === 1) {
+            p1Element.classList.toggle('my-turn');
+        } else {
+            p2Element.classList.toggle('my-turn');
+        }
+    })
+
+})();
+
+
 // Game Handler
 (function game() {
 
-    const player1 = createPlayer('Player 1', 1, 'x', true);
+    let status = false;
+    const player1 = createPlayer('Player 1', 1, 'x', false);
     const player2 = createPlayer('Player 2', 2, 'o', false);
     const players = [player1, player2]
 
     // TODO: write a coin toss mechanic to determine who goes first
-    let currentPlayer = player1;
+    let currentPlayer;
+
+    function coinToss() {
+        for (let player of players) {
+            player.resetTurn();
+        }
+        if (Math.floor(Math.random() * 2) === 0) {
+            currentPlayer = player1;
+        } else {
+            currentPlayer = player2;
+        }
+        currentPlayer.setTurn();
+
+        pubsub.publish('coinToss', currentPlayer.getplayerId())
+        
+        return currentPlayer
+    }
+
+    currentPlayer = coinToss();
+    console.log(currentPlayer.getName())
+    console.log(currentPlayer.getTurn())
+
 
     // Ordered to favor center indices as having a higher chance of being included
     // in a winning pattern
@@ -103,13 +219,14 @@ const pubsub = {
             return score
         }
         const setTurn = () => {return isTurn = !isTurn}
+        const resetTurn = () => {return isTurn = false}
 
         const resetScore = () => {
             score = 0;
             return score
         }
 
-        return {getName, getMark, getScore, getplayerId, setScore, getTurn, setTurn, resetScore}
+        return {getName, getMark, getScore, getplayerId, setScore, getTurn, setTurn, resetTurn, resetScore}
     }
 
 
@@ -120,6 +237,7 @@ const pubsub = {
                 currentPlayer = player;
             } 
         } 
+        pubsub.publish('turnChange', currentPlayer)
     }
 
 
@@ -206,85 +324,14 @@ const pubsub = {
             clearedBoard = gameBoard.clearBoard()
             pubsub.publish('gameStateChange', {clearedBoard, players})
         }
-        
+        coinToss()
     })
 
     return {takeTurn, revealBoard, showStats}
+    
 })();
 
-// UI Manager
-(function () {
-    // Locate important html elements
-    const gameControlsElement = document.querySelector('.game-controls');
-    const newGameBtn = document.querySelector('#new-game-btn');
-    const resetBtn = document.querySelector('#reset-btn');
-    const gameBoardElement = document.querySelector('#game-board');
-    const p1Element = document.querySelector('#player1');
-    const p2Element = document.querySelector('#player2');
 
-    // Listenters
-    gameBoardElement.addEventListener('click', (event) => {
-    let target = event.target;
-    let spaceIdx = target.dataset.spaceIdx;
-    pubsub.publish("spaceClicked", spaceIdx)
-    return spaceIdx
-    });
-
-    gameControlsElement.addEventListener('mousedown', (event) => {
-        let target = event.target;
-        target.classList.toggle('btn-dwn');
-    });
-
-    gameControlsElement.addEventListener('mouseup', (event) => {
-        let target = event.target;
-        target.classList.toggle('btn-dwn');
-        if(target.id === 'new-game-btn') {
-            pubsub.publish('btnPressed', 'newGame');
-        } else if (target.id === 'reset-btn') {
-            pubsub.publish('btnPressed', 'reset');
-        }
-        
-    });
-
-    const markSpace = (board) => {
-        for (let i = 0; i < board.length; i++){
-            let boardSpaceElement = document.querySelector(`#marker-space${i}`);
-            boardSpaceElement.classList.remove('x-marker', 'o-marker');
-            if (board[i]) {
-                boardSpaceElement.classList.toggle(`${board[i]}-marker`);
-            }
-        }};
-
-    const updateScore = (player) => {
-        let id = extractPlayerId(player);
-        let playerScore = document.querySelector(`#p${id}-score`);
-        playerScore.textContent = player.getScore()
-    }
-
-    function extractPlayerId(player) {
-        return player.getplayerId()
-    }
-
-    // Pubsub Subscriptions
-    pubsub.subscribe('boardChanged', (board) => {
-        markSpace(board)
-    });
-
-    pubsub.subscribe('win', (players) => {
-        players.forEach((player) => {
-            updateScore(player);
-        })
-    });
-
-    pubsub.subscribe('gameStateChange', (payload) => {
-        // updateScore(payload.players);
-        payload.players.forEach((player) => {
-            updateScore(player);
-        })
-        markSpace(payload.clearedBoard);
-    })
-
-})();
 
 
 
